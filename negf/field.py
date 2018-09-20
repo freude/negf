9af96f4550c13b9a57bd949becf7d7d0775c200a
Mykhailo Klymenko <mike.klymenko@rmit.edu.au>
2018-09-21 00:19:45 +1000

    positioning of the molecule

--- negf/field.py
+++ negf/field.py
@@ -56,10 +56,12 @@ class Field(object):
 
             data, meta = read_cube(path)
 
+            data = np.swapaxes(data, 1, 2)
+
             shape = data.shape
             x = np.linspace(meta['org'][0], meta['xvec'][0] * shape[0], shape[0])
-            y = np.linspace(meta['org'][1], meta['yvec'][1] * shape[1], shape[1])
-            z = np.linspace(meta['org'][2], meta['zvec'][2] * shape[2], shape[2])
+            z = np.linspace(meta['org'][1], meta['yvec'][1] * shape[1], shape[1])
+            y = np.linspace(meta['org'][2], meta['zvec'][2] * shape[2], shape[2])
 
             print(shape)
             print(meta)
@@ -69,15 +71,16 @@ class Field(object):
             print(meta['zvec'])
 
             self._cube = [[meta['org'][0]*0.529177,
-                           meta['org'][1]*0.529177,
-                           meta['org'][2]*0.529177],
+                           meta['org'][2]*0.529177,
+                           meta['org'][1]*0.529177],
                           [meta['xvec'][0] * shape[0]*0.529177 - meta['org'][0]*0.529177,
-                           meta['yvec'][1] * shape[1]*0.529177 - meta['org'][1]*0.529177,
-                           meta['zvec'][2] * shape[2]*0.529177 - meta['org'][2]*0.529177]]
+                           meta['zvec'][2] * shape[2]*0.529177 - meta['org'][2]*0.529177,
+                           meta['yvec'][1] * shape[1]*0.529177 - meta['org'][1]*0.529177]]
 
+            self.origin = (np.array(self._cube[1])-np.array(self._cube[0]))*0.25
             self._shape = shape
 
-        self._interpolant = RegularGridInterpolator((x, y, z), data)
+        self._interpolant = RegularGridInterpolator((x, y, z), data, bounds_error=False)
 
     def set_origin(self, origin):
 
@@ -87,19 +90,25 @@ class Field(object):
         self._origin_shift = origin
         self._origin_has_changed = True
 
-    def get_values(self, coords, translate=None):
+    def get_values(self, coords1, translate=None):
+
+        coords = coords1.copy()
 
         if len(coords.shape) < 2:
             coords = [coords]
 
-        if self._origin_has_changed:
-            for j in range(len(coords)):
+        for j in range(len(coords)):
+            coords[j] += self.origin
+
+            if self._origin_has_changed:
                 coords[j] -= self._origin_shift
 
-        values = self._interpolant(coords)
-        np.nan_to_num(values, copy=False)
+            if isinstance(translate, np.ndarray):
+                coords[j] -= translate
 
-        return values
+        values = self._interpolant(coords)
+
+        return np.nan_to_num(values)
 
 
 if __name__ == '__main__':
@@ -112,16 +121,24 @@ if __name__ == '__main__':
     y = np.linspace(fl._cube[0][1], fl._cube[1][1], fl._shape[1])
     z = np.linspace(fl._cube[0][2], fl._cube[1][2], fl._shape[2])
 
+    x = np.linspace(0, 60, 100)
+    y = np.linspace(0, 11, 100)
+    z = np.linspace(0, 5.5, 100)
+
     X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
 
-    data = fl.get_values(np.vstack((X.flatten(), Y.flatten(), Z.flatten())).T)
-    data = data.reshape(fl._shape)
-    plt.imshow(data[:, :, 50])
-    plt.show()
+    # data = fl.get_values(np.vstack((X.flatten(), Y.flatten(), Z.flatten())).T, translate=np.array([0,0,15]))
+    # data = data.reshape(X.shape)
+    # plt.imshow(data[:, :, 50])
+    # # plt.contour(data[:, 50, :], levels=(0.04, 0.06, 0.08, 0.1, 0.2))
+    # plt.show()
 
-    fl.set_origin([0, 5, 0])
+    fl.set_origin(np.array([20, 11, 2.75]))
 
     data = fl.get_values(np.vstack((X.flatten(), Y.flatten(), Z.flatten())).T)
-    data = data.reshape(fl._shape)
+    data = data.reshape(X.shape)
     plt.imshow(data[:, :, 50])
+
+    # plt.contour(data[:, 50, :], levels=(0.04, 0.06, 0.08, 0.1, 0.2))
+
     plt.show()
--- negf/test_gf_nw.py
+++ negf/test_gf_nw.py
@@ -4,64 +4,96 @@ import tb
 from field import Field
 
 
-tb.Atom.orbital_sets = {'Si': 'SiliconSP3D5S', 'H': 'HydrogenS'}
-h = tb.Hamiltonian(xyz='/home/mk/NEGF_project/SiNW.xyz', nn_distance=2.4)
-h.initialize()
-period = [0, 0, 5.50]
-h.set_periodic_bc([period])
-h_l, h_0, h_r = h.get_coupling_hamiltonians()
+def compute_tb_matrices(save=True):
 
-# energy = np.linspace(2.07, 2.3, 50)
-# energy = np.linspace(2.07, 2.3, 50) + 0.2
-# energy = np.linspace(-1.0, -0.3, 200)
-energy = np.concatenate((np.linspace(-0.8, -0.3, 100), np.linspace(2.05, 2.5, 100)))
-energy = np.linspace(2.05, 2.5, 50)
-# energy = np.linspace(2.3950, 2.4050, 20)
+    tb.Atom.orbital_sets = {'Si': 'SiliconSP3D5S', 'H': 'HydrogenS'}
+    h = tb.Hamiltonian(xyz='/home/mk/NEGF_project/SiNW.xyz', nn_distance=2.4)
+    h.initialize()
+    period = [0, 0, 5.50]
+    h.set_periodic_bc([period])
+    h_l, h_0, h_r = h.get_coupling_hamiltonians()
 
-# energy = energy[20:35]
+    coords = h.get_site_coordinates()
 
-sgf_l = []
-sgf_r = []
-factor = []
-factor1 = []
-factor2 = []
+    if save:
+        np.save('h_l', h_l)
+        np.save('h_0', h_0)
+        np.save('h_r', h_r)
+        np.save('coords', coords)
+
+    return h_l, h_0, h_r, coords
+
+
+# h_l, h_0, h_r, coords = compute_tb_matrices()
+
+h_l = np.load('h_l.npy')
+h_0 = np.load('h_0.npy')
+h_r = np.load('h_r.npy')
+coords = np.load('coords.npy')
+
+energy = np.linspace(2.05, 2.2, 50)
+
+
+def compute_self_energies_for_leads(energy, h_l, h_0, h_r, save=True):
+
+    sgf_l = []
+    sgf_r = []
+    factor = []
+    factor1 = []
+    factor2 = []
+
+    num_sites = h_0.shape[0]
+
+    for j, E in enumerate(energy):
+
+        L, R = tb.surface_greens_function(E, h_l, h_0, h_r)
+
+        test_gf = E * np.identity(num_sites) - h_0 - L - R
+        metrics = np.linalg.cond(test_gf)
+        print "{} of {}: energy is {}, metrics is {}".format(j + 1, energy.shape[0], E, metrics)
+
+        # if metrics > 15000:
+        #     R = iterate_gf(E, h_0, h_l, h_r, R, 1)
+        #     L = iterate_gf(E, h_0, h_l, h_r, L, 1)
+
+        sgf_l.append(L)
+        sgf_r.append(R)
+        # factor.append(phase)
+        # factor1.append(phase1)
+        # factor2.append(phase2)
+
+    sgf_l = np.array(sgf_l)
+    sgf_r = np.array(sgf_r)
+
+    if save:
+        np.save('sgf_l', sgf_l)
+        np.save('sgf_r', sgf_r)
+
+    return sgf_l, sgf_r
+
+
+# sgf_l, sgf_r = compute_self_energies_for_leads(energy, h_l, h_0, h_r, save=True)
+
+sgf_l = np.load('sgf_l.npy')
+sgf_r = np.load('sgf_r.npy')
+
 
 num_sites = h_0.shape[0]
 
-for j, E in enumerate(energy):
-    # L, R = surface_greens_function_poles_Shur(j, E, h_l, h_0, h_r)
-    L, R = tb.surface_greens_function(E, h_l, h_0, h_r)
+field = Field(path='/home/mk/gaussian_swarm/gauss_comp/out_ionized.cube')
+field.set_origin(np.array([6.36, 11.86+5, 2.75]))
 
-    test_gf = E * np.identity(num_sites) - h_0 - L - R
+period = np.array([0, 0, 5.50])
+values_m2 = field.get_values(coords, translate=-2*period)
+values_m1 = field.get_values(coords, translate=-period)
+values_0 = field.get_values(coords)
+values_1 = field.get_values(coords, translate=period)
+values_2 = field.get_values(coords, translate=2*period)
 
-    metrics = np.linalg.cond(test_gf)
-    print "{} of {}: energy is {}, metrics is {}".format(j + 1, energy.shape[0], E, metrics)
+eps = 11.7
 
-    # if metrics > 15000:
-    #     R = iterate_gf(E, h_0, h_l, h_r, R, 1)
-    #     L = iterate_gf(E, h_0, h_l, h_r, L, 1)
+shifts = np.concatenate((values_m2, values_m1, values_0, values_1, values_2)) / eps
 
-    sgf_l.append(L)
-    sgf_r.append(R)
-    # factor.append(phase)
-    # factor1.append(phase1)
-    # factor2.append(phase2)
-
-sgf_l = np.array(sgf_l)
-sgf_r = np.array(sgf_r)
-factor = np.array(factor)
-factor1 = np.array(factor1)
-factor2 = np.array(factor2)
-
-# field = Field(path='/home/mk/gaussian_swarm/gauss_comp/out_neutral.cube')
-# coords = h.get_site_coordinates()
-# period = np.array(period)
-# values_m2 = field.get_values(coords, translate=-2*period)
-# values_m1 = field.get_values(coords, translate=-period)
-# values_0 = field.get_values(coords)
-# values_1 = field.get_values(coords, translate=period)
-# values_2 = field.get_values(coords, translate=2*period)
-#
 zeros = np.zeros(h_0.shape)
 
 # h_d = np.block([[h_0 + np.diag(values_m2), h_l, zeros, zeros, zeros],
@@ -76,6 +108,10 @@ h_d = np.block([[h_0, h_l, zeros, zeros, zeros],
                 [zeros, zeros, h_r, h_0, h_l],
                 [zeros, zeros, zeros, h_r, h_0]])
 
+h_d += np.diag(shifts)
+
+damp = np.diag(np.zeros(np.diag(h_d).shape) + 1j*0.001)
+
 # gf = np.linalg.pinv(np.multiply.outer(energy, np.identity(2 * num_sites)) - h_device - selfenergy)
 
 # gf = np.linalg.pinv(np.multiply.outer(energy, np.identity(2*num_sites)) - h_d - sgf_l - sgf_r)
@@ -88,7 +124,7 @@ tr = np.zeros((energy.shape[0]), dtype=np.complex)
 dos = np.zeros((energy.shape[0]), dtype=np.complex)
 
 for j, E in enumerate(energy):
-    print(j)
+
     sgf_l_loc = np.block([[sgf_l[j, :, :], zeros, zeros, zeros, zeros],
                           [zeros, zeros, zeros, zeros, zeros],
                           [zeros, zeros, zeros, zeros, zeros],
@@ -101,13 +137,19 @@ for j, E in enumerate(energy):
                           [zeros, zeros, zeros, zeros, zeros],
                           [zeros, zeros, zeros, zeros, sgf_r[j, :, :]]])
 
-    gf = np.linalg.pinv(E * np.identity(5 * num_sites) - h_d - sgf_l_loc - sgf_r_loc)
+    gf = E * np.identity(5 * num_sites) - h_d - sgf_l_loc - sgf_r_loc
+
+    metrics = np.linalg.cond(gf)
+    print "{} of {}: energy is {}, metrics is {}".format(j + 1, energy.shape[0], E, metrics)
+
+    gf = np.linalg.pinv(gf)
 
     gf0 = np.matrix(gf)
     gamma_l = 1j * (np.matrix(sgf_l_loc) - np.matrix(sgf_l_loc).H)
     gamma_r = 1j * (np.matrix(sgf_r_loc) - np.matrix(sgf_r_loc).H)
     dos[j] = np.real(np.trace(1j * (gf0 - gf0.H)))
     tr[j] = np.real(np.trace(gamma_l * gf0 * gamma_r * gf0.H))
+
 
 ax = plt.axes()
 ax.set_xlabel('Energy (eV)')
