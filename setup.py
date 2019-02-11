f6bbbbd46333b43a7d5224c4b9dc4af610be3937
Mykhailo Klymenko <mike.klymenko@rmit.edu.au>
2019-02-11 17:03:44 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='negf',
+      version='0.1',
+      description='Python framework for negf computations',
+      author='Mike Klymenko',
+      author_email='mike.klymenko@rmit.edu.au',
+      license='MIT',
+      packages=['negf'],
+      zip_safe=False
+      )
